5e091a2260d1c5b7219671886b10fa2f2d3d85ea
Vincent Siu <vince.siu4@gmail.com>
2016-10-02 21:48:58 -0700

    Beef up the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
-#!/bin/bash -xue
+#!/bin/bash
 
 rsync -a ./calculator.php ~/www/calculator.php
+
+if [[ "$1" == '--submit' ]]; then
+    mkdir 904280752
+    cp {team.txt,readme.txt,calculator.php} 904280752
+    zip -r P1A 904280752/
+    rm -rf 904280752
+    source <(curl -sL http://oak.cs.ucla.edu/classes/cs143/project/p1a_test) 904280752
+fi
