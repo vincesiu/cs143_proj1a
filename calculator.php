9babeab0e59537ea5760d535da2dc2e85df8706e
Vincent Siu <vince.siu4@gmail.com>
2016-10-05 00:12:58 -0700

    Clean up comments in main php file

--- calculator.php
+++ calculator.php
@@ -59,11 +59,11 @@
 			<h3 class="inline">Result:</h3>
 			<p class="inline">
 				<?php 
-					// TODO: error check (invalid chars, div by zero)
-					// TODO: evaluate
+
                     $ret="DefaultValue";
+
                     if (preg_match('/[^0-9.*\/+\-\s]/', $equ)) {
-                       //catches invalid characters
+                        //catches invalid characters
                         //list of valid characters:
                         //  1. 0-9
                         //  2. .
@@ -71,15 +71,20 @@
                         //  4. /*
                         //  5. whitespace
                         $ret="Invalid Expression!";
+
                     } elseif (preg_match('/\/\s*0/', $equ)) {
                         //division by zero
                         $ret = "Division by zero error!";
+
                     } elseif (preg_match('/\*\*/', $equ)) {
                         //catching an exponentiation operator
                         $ret = "Invalid Expression!";
+
                     } else {
                         $ret = eval("return $equ;");
                         if ($ret == FALSE) {
+                            //catches if the mathematical expression
+                            //is improperly formed, eg. 1++1
                             $ret = "Invalid Expression!";
                         } else {
 					        $ret = $equ . " = " . $ret; 
